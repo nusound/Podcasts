1d28858dd70c82c35a2cc071a49d71e505e008e5
uros <uroshni77@gmail.com>
2022-09-30 14:59:27 +0200

    generated controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  get 'about', to: 'pages#about'
+  get 'pricing', to: 'pages#pricing'
+  get 'contact', to: 'pages#contact'
+
+  root to: 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
